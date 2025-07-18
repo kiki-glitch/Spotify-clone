aed72fd2879f8364c5a225819c647302c5af4d5a
unknown <kikialex37@gmail.com>
2025-07-18 20:42:46 +0300

    Sptify clone part 2

--- backend/spotify_clone/settings.py
+++ backend/spotify_clone/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'spotify_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
